d7904da207007e63266904c4414f3cf17cef55f9
tahseen-algo75 <110347211+tahseen-algo75@users.noreply.github.com>
2024-02-08 13:02:22 -0500

    Update CloneNotSupportedException.java

--- CloneNotSupportedException.java
+++ CloneNotSupportedException.java
@@ -1,11 +1,4 @@
-package Homework1;
 
-/*
- * @author Tahseen Zaman
- *  ID : 114332480
- *  Recitation : 03
- */
- 
 public class CloneNotSupportedException extends Exception {
 	
 	public CloneNotSupportedException() {}
